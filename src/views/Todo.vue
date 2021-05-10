829aca10bd811bc33178863991c9c74d30c4a615
Tristin Hewett <thewett-c@sbgtv.com>
2021-05-10 12:45:01 -0700

    Add addTask method, no tasks display

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="home">
-    <v-list flat class="pt-0">
+    <v-text-field
+      v-model="newTaskTitle"
+      clearable
+      hide-details
+      class="pa-3"
+      outlined
+      label="Add Task"
+      append-icon="mdi-plus"
+      @click:append="addTask"
+      @keydown.enter="addTask"
+    ></v-text-field>
+    <v-list v-if="tasks.length" flat class="pt-0">
       <div v-for="task in tasks" :key="task.id">
         <v-list-item @click="doneTask(task.id)">
           <template v-slot:default>
             <v-list-item-action>
               <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
             </v-list-item-action>
-
             <v-list-item-content>
-              <v-list-item-title :class="{ 'text-decoration-line-through' : task.done }">{{ task.title }}</v-list-item-title>
+              <v-list-item-title :class="{ 'text-decoration-line-through': task.done }">{{
+                task.title
+              }}</v-list-item-title>
             </v-list-item-content>
             <v-list-item-action>
               <v-btn @click.stop="deleteTask(task.id)" icon>
@@ -21,6 +33,10 @@
         <v-divider></v-divider>
       </div>
     </v-list>
+    <div class="dead-center" v-else>
+      <v-icon size="100" color="primary">mdi-check</v-icon>
+      <div class="text-h5 primary--text">No tasks</div>
+    </div>
   </div>
 </template>
 
@@ -29,31 +45,57 @@ export default {
   name: "Todo",
   data() {
     return {
+      newTaskTitle: "",
       tasks: [
         {
           id: 1,
-          title: 'Wake up',
+          title: "Wake up",
           done: false,
-        },        {
+        },
+        {
           id: 2,
-          title: 'Brush teeth',
+          title: "Brush teeth",
           done: false,
-        },        {
+        },
+        {
           id: 3,
-          title: 'Drink Coffee',
+          title: "Drink Coffee",
           done: false,
-        }
-      ]
-    }
+        },
+      ],
+    };
   },
   methods: {
+    addTask() {
+      if (this.newTaskTitle && this.newTaskTitle.length) {
+        if (this.tasks.some((task) => task.title === this.newTaskTitle)) {
+          console.log(
+            "Hey, you already have a task called ",
+            this.newTaskTitle,
+            ", ding-dong"
+          );
+          return;
+        }
+        const arrOfIDs = this.tasks.map((task) => task.id);
+        const highestNumber = Math.max(...arrOfIDs);
+        const newTaskObj = {
+          id: highestNumber + 1,
+          title: this.newTaskTitle,
+          done: false,
+        };
+        this.tasks.push(newTaskObj);
+        this.newTaskTitle = "";
+      } else {
+        console.log("you need to enter a new task title first...dork");
+      }
+    },
     deleteTask(id) {
-      let tasks = [...this.tasks]
-      tasks = tasks.filter(task => task.id !== id);
+      let tasks = [...this.tasks];
+      tasks = tasks.filter((task) => task.id !== id);
       this.tasks = tasks;
     },
     doneTask(id) {
-      const [task] = this.tasks.filter(task => task.id === id);
+      const [task] = this.tasks.filter((task) => task.id === id);
       if (task) {
         task.done = !task.done;
       }
@@ -61,3 +103,13 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+  .dead-center {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    opacity: .5;
+  }
+</style>
